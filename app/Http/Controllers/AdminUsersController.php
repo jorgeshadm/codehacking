36000815ec709f0061912438d0368ffce1a1b83e
Jorge Shimada Laravel <jorge_shot@outlook.com>
2018-08-31 18:21:00 -0300

    able to create post with static cateogory

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -11,6 +11,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -79,7 +80,6 @@ class AdminUsersController extends Controller
 
         return redirect('admin/users');
 
-//        return $request->all();
     }
 
     /**
@@ -166,5 +166,16 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        unlink(public_path() . $user->photo->file);
+
+        $user->delete();
+
+        Session::flash('deleted_user', 'The user has been deleted');
+
+        return redirect('/admin/users');
+
     }
 }
